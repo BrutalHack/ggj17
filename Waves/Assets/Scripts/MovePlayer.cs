188a0f4572e0c3d280d2d0fa6c118280580b45dd
Eike Wulff <e.wulff@aleron.eu>
2017-01-21 18:12:10 +0100

    rotation wip

--- Waves/Assets/Scripts/MovePlayer.cs
+++ Waves/Assets/Scripts/MovePlayer.cs
@@ -2,79 +2,45 @@
 
 public class MovePlayer : MonoBehaviour
 {
-    private float _time;
-    private Vector3 _targetVector = Vector3.up;
-    private Vector3 _previousDirection = Vector3.right;
-    private bool _transitioning;
-    private bool _up = true;
+    private bool _movingUp;
     public float Speed = 1;
     public float AngularSpeed = 1;
-    private float _Angle;
+    private Rigidbody2D _rigidbody;
+    public float _maxAngle = 60;
+    private float targetAngle;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
-        _time = Time.time;
+        targetAngle = 60;
+        _rigidbody.velocity = Vector3.right;
+        _movingUp = true;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (!_transitioning)
-            {
-                _previousDirection = GetDirection();
-                _targetVector = Vector3.right;
-                _time = Time.time;
-                _transitioning = true;
-            }
-            else
-            {
-                _previousDirection = GetDirection();
-                _time = Time.time;
-                _transitioning = false;
-                if (_up)
-                {
-                    _targetVector = Vector3.up;
-                }
-                else
-                {
-                    _targetVector = Vector3.down;
-                }
-            }
-            _Angle = Vector3.Angle(_previousDirection, _targetVector);
-        }
-        if (_transitioning)
-        {
-            Vector3 direction = GetDirection();
-            Debug.Log("Trans: " + Vector3.Angle(direction, _targetVector));
-            transform.position += direction * Time.deltaTime * Speed;
-            if (Vector3.Angle(direction, _targetVector) < 5f)
-            {
-                _time = Time.time;
-                _previousDirection = Vector3.right;
-                _up = !_up;
-                if (_up)
-                {
-                    _targetVector = Vector3.up;
-                }
-                else
-                {
-                    _targetVector = Vector3.down;
-                }
-                _transitioning = false;
-            }
-        }
-        else
-        {
-            Vector3 direction = GetDirection();
-            Debug.Log(Vector3.Angle(direction, _targetVector));
-            transform.position += direction * Time.deltaTime * Speed;
+            targetAngle = -targetAngle;
+            _movingUp = !_movingUp;
         }
     }
 
-    private Vector3 GetDirection()
+    void FixedUpdate()
     {
-        return Vector3.Slerp(_previousDirection, _targetVector,
-            (Time.time - _time) / _Angle * 90 / (1f / AngularSpeed) * Speed);
+        Debug.Log(_rigidbody.rotation);
+        if (_movingUp && (_rigidbody.rotation < 60 || _rigidbody.rotation > 270))
+        {
+            _rigidbody.MoveRotation((_rigidbody.rotation + targetAngle) * Time.fixedDeltaTime * AngularSpeed);
+        }
+        else if (!_movingUp && (_rigidbody.rotation < 90 || _rigidbody.rotation > 300))
+        {
+            _rigidbody.MoveRotation((_rigidbody.rotation + targetAngle) * Time.fixedDeltaTime * AngularSpeed);
+        }
+        _rigidbody.velocity = transform.right * Speed;
     }
 }
