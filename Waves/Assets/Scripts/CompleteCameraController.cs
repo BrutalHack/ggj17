84cbe6b17c5bd4bbd5e57626091465c5daefcf08
Eike Wulff <e.wulff@aleron.eu>
2017-01-21 14:28:20 +0100

    player movement gebaut

---
+++ Waves/Assets/Scripts/CompleteCameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CompleteCameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Waves/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float _time;
+    private Vector3 _targetVector = Vector3.up;
+    private Vector3 _previousDirection = Vector3.right;
+    private bool _transitioning;
+    private bool _up = true;
+    public float Speed = 1;
+    public float AngularSpeed = 1;
+    private float _Angle;
+
+    void Start()
+    {
+        _time = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!_transitioning)
+            {
+                _previousDirection = GetDirection();
+                _targetVector = Vector3.right;
+                _time = Time.time;
+                _transitioning = true;
+            }
+            else
+            {
+                _previousDirection = GetDirection();
+                _time = Time.time;
+                _transitioning = false;
+                if (_up)
+                {
+                    _targetVector = Vector3.up;
+                }
+                else
+                {
+                    _targetVector = Vector3.down;
+                }
+            }
+            _Angle = Vector3.Angle(_previousDirection, _targetVector);
+        }
+        if (_transitioning)
+        {
+            Vector3 direction = GetDirection();
+            Debug.Log("Trans: " + Vector3.Angle(direction, _targetVector));
+            transform.position += direction * Time.deltaTime * Speed;
+            if (Vector3.Angle(direction, _targetVector) < 5f)
+            {
+                _time = Time.time;
+                _previousDirection = Vector3.right;
+                _up = !_up;
+                if (_up)
+                {
+                    _targetVector = Vector3.up;
+                }
+                else
+                {
+                    _targetVector = Vector3.down;
+                }
+                _transitioning = false;
+            }
+        }
+        else
+        {
+            Vector3 direction = GetDirection();
+            Debug.Log(Vector3.Angle(direction, _targetVector));
+            transform.position += direction * Time.deltaTime * Speed;
+        }
+    }
+
+    private Vector3 GetDirection()
+    {
+        return Vector3.Slerp(_previousDirection, _targetVector,
+            (Time.time - _time) / _Angle * 90 / (1f / AngularSpeed) * Speed);
+    }
+}
