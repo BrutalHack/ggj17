abf0673bdf57493e225cc2bfa5ebc5b85f71cadf
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2017-01-21 19:45:13 +0100

    Created start scene.

---
+++ Waves/Assets/Scripts/StartManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartGame();
+        }
+        else if (Input.GetKey(KeyCode.Escape))
+        {
+            CloseGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
