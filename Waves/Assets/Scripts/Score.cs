69f959f3689f26c985f4056ce5997153fab8d8bd
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2017-01-22 09:28:52 +0100

    Create Score and UI for PC nad Mobile

---
+++ Waves/Assets/Scripts/Score.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public GameObject Player;
+    public Text ScoreText;
+    private float _startPositionX;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _startPositionX = CurrentPositionX();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ScoreText.text = GetScoreAsString();
+	}
+
+    private float CurrentPositionX()
+    {
+        return Player.transform.position.x;
+    }
+
+    private String GetScoreAsString()
+    {
+        float score = CurrentPositionX() - _startPositionX;
+        if (score < 1)
+        {
+            return "0";
+        }
+        return score.ToString("n2");
+    }
+}
