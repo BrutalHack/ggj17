bf89fb3686c1b927beb22e3437692e5b7e421629
Benjamin Justice <benjamin.justice@brutalhack.com>
2017-01-22 14:28:00 +0100

    start screen

--- Waves/Assets/Scripts/StartManager.cs
+++ Waves/Assets/Scripts/StartManager.cs
@@ -3,9 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
+    public GameObject DesktopUI;
+
+    public GameObject MobileUI;
+
     // Use this for initialization
     void Start()
     {
+        if (
+            Application.platform == RuntimePlatform.Android ||
+            Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.WSAPlayerARM ||
+            Application.platform == RuntimePlatform.WSAPlayerX64 ||
+            Application.platform == RuntimePlatform.WSAPlayerX86
+        )
+        {
+            DesktopUI.SetActive(false);
+            MobileUI.SetActive(true);
+        }
     }
 
     // Update is called once per frame
